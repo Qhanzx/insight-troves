33efff75307a6944aed69b5babb39597c5e14dee
Qhanzx <huqkazanx@gmail.com>
2024-01-08 17:52:07 +0000

    building html

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1>Hello</h1>'
+    return render_template('post.html')
+
+
+
+#https://www.behance.net/gallery/173695679/Blog-Website-Landing-Page-Design?tracking_source=search_projects&l=35
